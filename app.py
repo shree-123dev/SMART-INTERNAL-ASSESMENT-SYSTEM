b96a18c4329204c9a6f0014dd998bb1cf09f689b
vu2f2425005@pvppcoe.ac.in <vu2f24254005@pvppcoe.ac.in>
2026-07-28 20:28:59 +0530

    added authentication page and flask routing

--- app.py
+++ app.py
@@ -14,5 +14,45 @@ def login():
     return render_template("login.html")
 
 
+# ---------------- SIGNUP PAGE ----------------
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+# ---------------- FORGOT PASSWORD ----------------
+@app.route("/forgot_password")
+def forgot_password():
+    return render_template("forgot_password.html")
+
+
+#------------------DASHBOARD------------------------
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+#-----------------STUDENT------------------
+@app.route("/student")
+def student():
+    return render_template("student.html")
+
+#----------------TEACHER----------------
+@app.route("/teacher")
+def teacher():
+    return render_template("teacher.html")
+
+#--------------ADMIN------------------
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+#---------------ABOUT------------------
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
